1f3c07db8a15f67fce3b32446962fe1d86fb90e0
unknown <anamulislamshamim117@gmail.com>
2022-12-29 14:07:26 +0600

    api calling with redux, using redux-thunk, redux-logger, axios

---
+++ index.js
@@ -0,0 +1,91 @@
+// async action -api calling
+// api url - https://jsonplaceholder.typicode.com/todos
+// middleware - redux-thunk
+// axios api
+
+const { default: axios } = require("axios");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
+const { default: thunk } = require("redux-thunk");
+
+// constants
+const GET_TODOS_REQUEST = "GET_TODOS_REQUEST";
+const GET_TODOS_SUCCESS = "GET_TODOS_SUCCESS";
+const GET_TODOS_FAILED = "GET_TODOS_FAILED";
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+// states
+const initialTodosState = {
+    todos:[],
+    isLoading:false,
+    error:null,
+};
+
+// actions
+const getTodosRequest = () => {
+    return {
+        type:GET_TODOS_REQUEST,
+    };
+};
+const getTodosFailed = (error) => {
+    return {
+        type:GET_TODOS_FAILED,
+        payload: error,
+    };
+};
+const getTodosSuccess = (data) => {
+    return {
+        type:GET_TODOS_SUCCESS,
+        payload: data,
+    }
+};
+
+// reducer 
+const reducer = (state= initialTodosState, action) => {
+    switch(action.type){
+        case GET_TODOS_REQUEST:
+            return {
+                ...state,
+                isLoading:true,
+            };
+        case GET_TODOS_FAILED:
+            return {
+                ...state,
+                isLoading:false,
+                error:action.payload,
+            };
+        case GET_TODOS_SUCCESS:
+            return {
+                ...state,
+                isLoading:false,
+                error:null,
+                todos:action.payload
+            };
+        default:
+            return state;
+    }
+};
+
+// async action creator : fetchData();
+const fetchData = () => {
+    return (dispatch) => {
+        dispatch(getTodosRequest());
+        axios.get(API_URL)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+    }
+}
+// store
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
+// store.subscribe()
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// dispatch()
+store.dispatch(fetchData());
